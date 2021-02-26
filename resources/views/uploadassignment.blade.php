783bb6fedfd01ed436d2dcf4cf2e1216503eefb6
Marisna <dhila.risna@gmail.com>
2021-02-27 06:35:27 +0700

    Update uploadassignment.blade.php

--- resources/views/uploadassignment.blade.php
+++ resources/views/uploadassignment.blade.php
@@ -2,51 +2,54 @@
 
 @section('main-content')
     <!-- Page Heading -->
-    <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="/home">Home</a></li>
-        <li class="breadcrumb-item"><a href="/assignment">Assignment</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Upload Assignment</li>
-    </ol>
-    </nav>
     <h1 class="h3 mb-4 text-gray-800">{{ __('Upload Assignment') }}</h1>
 
     <!-- Main Content goes here -->
-    <div class="card shadow mb-4">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="form-group focused">
-                        <label class="form-control-label" for="attachement">Attachment</label>
-                        <form action="{{ route('dropzone') }}" class="dropzone" method="post" enctype="multipart/form-data">@csrf
-                        </form>
-                        <p class="text-form">(* File type : .docx, .pptx, .xlsx, .pdf)</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="form-group focused">
-                        <label class="form-control-label" for="description">Description</label>
-                        <textarea id="description" class="form-control" name="description" placeholder="Write a detailed description..."></textarea>
-                        <p class="text-form">You will be able to edit this information later.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-1">
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-col">Upload</button>
-                    </div>
-                </div>
-                <div class="col-auto">
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-new-cancel">Cancel</button>
-                    </div>
+    <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">File Upload</div>
+                <div class="card-body">
+                {{-- <form method="POST" action="{{ route('file.upload') }}" aria-label="{{ __('Upload') }}"> --}}
+                <form method="POST" action="#" aria-label="{{ __('Upload') }}">        
+                    @csrf
+                        <div class="form-group row">
+                            <label for="overview" class="col-sm-2 col-form-label text-md-right">{{ __('Attachment') }}</label>
+                            <div class="col-md-8">
+                                <textarea id="overview" cols="10" rows="10" class="form-control{{ $errors->has('overview') ? ' is-invalid' : '' }}" name="overview" value="{{ old('overview') }}" required autofocus></textarea>
+                                @if ($errors->has('overview'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('overview') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="price" class="col-md-2 col-form-label text-md-right">{{ __('Description') }}</label>
+                            <div class="col-md-8">
+                                <input id="price" type="text" class="form-control{{ $errors->has('price') ? ' is-invalid' : '' }}" name="price" required>
+                                @if ($errors->has('price'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('price') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-2">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Upload') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
     <!-- End of Main Content -->
